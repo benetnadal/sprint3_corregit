e1c0bb5d1cb6e1f7d857b11609f7b5befb0bb031
benetnadal <benetnadal@gmail.com>
2023-02-20 17:31:29 +0100

    nivell 1 exercicis 1-7

---
+++ js/checkout.js
@@ -0,0 +1,296 @@
+// Exercise 7
+
+
+function validate() {
+
+	var error = 0;
+
+	// Get the input fields
+	var fName = document.getElementById("fName");
+	var fEmail = document.getElementById("fEmail");
+
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
+
+	// Get the error elements
+
+	var errorName = document.getElementById("errorName"); //This field is required and must have, at least, 3 characters
+	var errorEmail = document.getElementById("errorEmail");
+	
+	var errorAddress = document.getElementById("errorAddress");
+	var errorLastN = document.getElementById("errorLastN");
+	var errorPassword = document.getElementById("errorPassword");
+	var errorPhone = document.getElementById("errorPhone");
+
+	// Validate fields entered by the user: name, phone, password, and email
+
+	/**************************************** FUNCIÓ NOMÉS LLETRES ************************/
+
+	function nomesLletres(string) {
+		var x = false;
+		var filtre = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+		var i = 0;
+		console.log(filtre.indexOf(string.charAt(i)));
+
+		while (!x && i < string.length) {
+
+			if (filtre.indexOf(string.charAt(i)) >= 0) {
+				i++;
+			} else x = true;
+		}
+		return x;
+	}
+
+	/********************************************************************************************************* */
+
+	/**************************************** FUNCIÓ NOMÉS NÚMEROS ************************/
+
+	function nomesNumeros(string) {
+		var x = false;
+		var filtre = '0123456789';
+		var i = 0;
+		//console.log(filtre.indexOf(string.charAt(i)));
+		while (!x && i < string.length) {
+
+			if (filtre.indexOf(string.charAt(i)) >= 0) {
+				i++;
+			} else x = true;
+		}
+		return x;
+	}
+
+	/********************************************************************************************************* */
+
+
+
+	/**************************************** FUNCIÓ LLETRES I NÚMEROS  ************************/
+
+	function conteLletres(string) {
+
+		var i = 0;
+		var y = false;
+		var filtre1 = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+
+		while (!y && i < string.length) {
+
+			if (filtre1.indexOf(string.charAt(i)) < 0) {
+				i++;
+			} else y = true;
+		}
+		return y;
+	}
+
+	function conteNumeros(string) {
+
+		var i = 0;
+		var z = false;
+		var filtre2 = '0123456789';
+
+		while (!z && i < string.length) {
+
+			if (filtre2.indexOf(string.charAt(i)) < 0) {
+				i++;
+			} else z = true;
+		}
+		return z;
+	}
+
+	/********************************************************************************************************* */
+
+	/**************************************** FUNCIÓ MAIL  ************************/
+
+	function mail(string) {
+		var matriu = string.split('');
+		var i = 0;
+		var arroba = 0;
+
+		for (i = 0; i < matriu.length; i++) {
+
+			if (matriu[i] == "@") {
+				arroba++;
+			}
+		}
+		return arroba
+	}
+
+	/********************************************************************************************************* */
+
+
+	// - Tots els camps són obligatoris:
+
+	// - Tots els camps han de tenir almenys 3 caràcters:
+
+	// - El nom i cognoms han de contenir només lletres:
+
+
+	/****** Nom ********************/
+
+	if (fName.value == "") {
+		error++
+		errorName.innerHTML = "This field is required";
+		errorName.style.color = "red";
+		errorName.style.fontSize = "larger";
+		fName.style.border = "thick solid red";
+
+	} else if (fName.value.length > 0 && fName.value.length < 3) {
+		error++;
+		errorName.innerHTML = "This field must have, at least, 3 characters";
+		errorName.style.color = "red";
+		errorName.style.fontSize = "larger";
+		fName.style.border = "thick solid red";
+
+	} else if ((nomesLletres(fName.value) == true)) {
+		error++;
+		errorName.innerHTML = "This field must contain only letters";
+		errorName.style.color = "red";
+		errorName.style.fontSize = "larger";
+		fName.style.border = "thick solid red";
+
+	} else {
+		fName.style.border = "thick solid green";
+		errorName.innerHTML = "";
+	}
+
+	/****** Cognom ********************/
+
+	if (fLastN.value == "") {
+		error++
+		errorLastN.innerHTML = "This field is required";
+		errorLastN.style.color = "red";
+		errorLastN.style.fontSize = "larger";
+		fLastN.style.border = "thick solid red";
+
+	} else if (fLastN.value.length > 0 && fLastN.value.length < 3) {
+		error++;
+		errorLastN.innerHTML = "This field must have, at least, 3 characters";
+		errorLastN.style.color = "red";
+		errorLastN.style.fontSize = "larger";
+		fLastN.style.border = "thick solid red";
+
+	} else if ((nomesLletres(fLastN.value) == true)) {
+		error++;
+		errorLastN.innerHTML = "This field must contain only letters";
+		errorLastN.style.color = "red";
+		errorLastN.style.fontSize = "larger";
+		fLastN.style.border = "thick solid red";
+
+	} else {
+		fLastN.style.border = "thick solid green";
+		errorLastN.innerHTML = "";
+	}
+
+
+// - L'email ha de tenir format d'email:
+
+if (fEmail.value == "") {
+	error++;
+	errorEmail.innerHTML = "This field is required";
+	errorEmail.style.color = "red";
+	errorEmail.style.fontSize = "larger";
+	fEmail.style.border = "thick solid red";
+
+} else if (fEmail.value.length > 0 && fEmail.value.length < 3) {
+	error++;
+	errorEmail.innerHTML = "This field must have, at least, 3 characters";
+	errorEmail.style.color = "red";
+	errorEmail.style.fontSize = "larger";
+	fEmail.style.border = "thick solid red";
+
+} else if (mail(fEmail.value) == 0) {
+	error++
+	errorEmail.innerHTML = "This field must contain an '@'";
+	errorEmail.style.color = "red";
+	errorEmail.style.fontSize = "larger";
+	fEmail.style.border = "thick solid red";
+
+} else if (mail(fEmail.value) > 0) {
+	fEmail.style.border = "thick solid green";
+	errorEmail.innerHTML = "";
+}
+
+// - La contrasenya ha d'incloure números i lletres:
+
+if (fPassword.value == "") {
+	errorPassword.innerHTML = "This field is required";
+	errorPassword.style.color = "red";
+	errorPassword.style.fontSize = "larger";
+	fPassword.style.border = "thick solid red";
+
+} else if (fPassword.value.length > 0 && fPassword.value.length < 3 || fPassword.value.length >8) {
+	error++;
+	errorPassword.innerHTML = "This field must have, at least, 3 char. and 8 char. max";
+	errorPassword.style.color = "red";
+	errorPassword.style.fontSize = "larger";
+	fPassword.style.border = "thick solid red";
+
+} else if (conteLletres(fPassword.value) == false || conteNumeros(fPassword.value) == false) {
+	error++;
+	errorPassword.innerHTML = "This field must contain letters and numbers";
+	errorPassword.style.color = "red";
+	errorPassword.style.fontSize = "larger";
+	fPassword.style.border = "thick solid red";
+
+} else {
+	fPassword.style.border = "thick solid green";
+	errorPassword.innerHTML = "";
+}
+
+// - Adreça:
+
+	if (fAddress.value == "") {
+		error++;
+		errorAddress.innerHTML = "This field is required";
+		errorAddress.style.color = "red";
+		errorAddress.style.fontSize = "larger";
+		fAddress.style.border = "thick solid red";
+
+	} else if (fAddress.value.length > 0 && fAddress.value.length < 3) {
+		error++;
+		errorAddress.innerHTML = "This field must have, at least, 3 characters";
+		errorAddress.style.color = "red";
+		errorAddress.style.fontSize = "larger";
+		fAddress.style.border = "thick solid red";
+
+	}else {
+		fAddress.style.border = "thick solid green";
+		errorAddress.innerHTML = "";
+	}
+
+	// - El telèfon ha de contenir només números:
+
+	if (fPhone.value == "") {
+		error++;
+		errorPhone.innerHTML = "This field is required";
+		errorPhone.style.color = "red";
+		errorPhone.style.fontSize = "larger";
+		fPhone.style.border = "thick solid red";
+
+	} else if (fPhone.value.length > 0 && fPhone.value.length < 9 || fPhone.value.length > 9) {
+		error++;
+		errorPhone.innerHTML = "This field must have 9 characters";
+		errorPhone.style.color = "red";
+		errorPhone.style.fontSize = "larger";
+		fPhone.style.border = "thick solid red";
+
+	} else if ((nomesNumeros(fPhone.value) == true)) {
+		error++;
+		errorPhone.innerHTML = "This field must contain only numbers with no spaces";
+		errorPhone.style.color = "red";
+		errorPhone.style.fontSize = "larger";
+		fPhone.style.border = "thick solid red";
+
+	} else {
+		fPhone.style.border = "thick solid green";
+		errorPhone.innerHTML = "";
+	}
+
+	//****************************************************************************************************** */
+
+	if (error > 0) {
+		return false;
+	} else {
+		return true;
+	}
+}
---
+++ js/shop.js
@@ -0,0 +1,326 @@
+// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
+const products = [{
+        id: 1,
+        name: 'cooking oil',
+        price: 10.5,
+        type: 'grocery'
+        /* offer: {
+            number: 3,
+            percent: 20 
+        }*/
+    },
+    {
+        id: 2,
+        name: 'Pasta',
+        price: 6.25,
+        type: 'grocery'
+    },
+    {
+        id: 3,
+        name: 'Instant cupcake mixture',
+        price: 5,
+        type: 'grocery',
+        /* offer: {
+            number: 10,
+            percent: 30
+        } */
+    },
+    {
+        id: 4,
+        name: 'All-in-one',
+        price: 260,
+        type: 'beauty'
+    },
+    {
+        id: 5,
+        name: 'Zero Make-up Kit',
+        price: 20.5,
+        type: 'beauty'
+    },
+    {
+        id: 6,
+        name: 'Lip Tints',
+        price: 12.75,
+        type: 'beauty'
+    },
+    {
+        id: 7,
+        name: 'Lawn Dress',
+        price: 15,
+        type: 'clothes'
+    },
+    {
+        id: 8,
+        name: 'Lawn-Chiffon Combo',
+        price: 19.99,
+        type: 'clothes'
+    },
+    {
+        id: 9,
+        name: 'Toddler Frock',
+        price: 9.99,
+        type: 'clothes'
+    }
+]
+// Array with products (objects) added directly with push(). Products in this array are repeated.
+const cartList = [];
+
+// Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
+const cart = [];
+const total = 0;
+
+
+//-------------------------------------------------------
+
+function Carts(name, price, type, quantity, subtotal, subtotalWithDiscount) {
+    this.name = name;
+    this.price = price;
+    this.type = type;
+    this.quantity = quantity;
+    this.subtotal = subtotal;
+    this.subtotalWithDiscount = subtotalWithDiscount;
+}
+
+// ************************************************ Nivell I *****************************************************
+
+// Exercici 1 -------------------------------------
+
+// 1. Loop for to the array products to get the item to add to cart
+// 2. Add found product to the cartList array
+
+/*  La funció que has de completar es diu buy(), la qual rep l'id del producte a afegir. 
+ Has de buscar el producte utilitzant aquest id rebut a l'array products, per finalment afegir-ho 
+   a l´array cartList. */
+
+
+function buy(id) {
+
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id == id) {
+            cartList.push(products[i]);
+            //console.log(cartList);
+        }
+    }
+}
+
+// Exercise 2 --------------------------------------------------------------------
+
+
+/* Ara implementarem una funció que permeti a l'usuari/ària eliminar l'array generat a l'anterior exercici: buidar el carret.
+En aquest cas, hauràs d'emprar la funció cleanCart(), la qual ha de reinicialitzar la variable CartList. */
+
+function cleanCart() {
+
+    cartList.splice(0, cartList.length); //Buidem cartList
+    console.log(cartList); // comprovem que es buidi
+
+    //Esborrem els productes de la finestra modal:
+
+    let comptador = 1;
+    let totalPrice = 0;
+
+    for (i = 0; i < cart.length; i++) {
+
+        document.getElementById(`producte${comptador}`).innerHTML = "";
+        document.getElementById(`preu${comptador}`).innerHTML = "";
+        document.getElementById(`quantitat${comptador}`).innerHTML = "";
+        document.getElementById(`total${comptador}`).innerHTML = "";
+        comptador++
+    }
+    document.getElementById("total_price").innerHTML = totalPrice; 
+    document.getElementById("count_product").innerHTML = "0"; 
+    cart.splice(0, cart.length); ////Buidem cart
+}
+
+
+
+
+// Exercise 3 ----------------------------------------------------------------
+
+// Calculate total price of the cart using the "cartList" array
+// S'ha d'implementar un bucle for per anar sumant l'import de tots els productes.
+
+function calculateTotal() {
+    let total = 0;
+    for (let i = 0; i < cartList.length; i++) {
+        total = total + cartList[i].price;
+        console.log(total)
+    }
+}
+
+// Exercise 4 ---------------------------------------------------------------------------------------------
+
+// Using the "cartlist" array that contains all the items in the shopping cart, 
+// generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+
+/* Seria més convenient que no apareguessin repetits, sinó que cada producte del carret comptés amb un camp quantitat.
+
+Per a això, hauràs de completar la funció generateCart(), la qual rep l'array cartList, generant l'array cart.
+
+Crea un botó per tal de poder cridar a la funció generateCart() */
+
+
+function generateCart() {
+
+    //He posat a index.html la matriu cartList ------> generateCart(cartList)
+
+    if (cartList.length > 0) { //No podem generar cart si cartList és buida
+
+        if (cart.length === 0) {
+
+            let nouObjecte = new Carts(cartList[0].name, cartList[0].price, cartList[0].type, 1, cartList[0].price, cartList[0].price);
+            cart.push(nouObjecte);
+            console.log(cart);
+
+            for (let i = 1; i < cartList.length; i++) {
+
+                let nameMatriu = cartList[i].name;
+
+                const index = cart.findIndex(element => {
+                    if (element.name === nameMatriu) {
+                        return true;
+                    } else
+                        return false;
+                });
+
+                console.log(index);
+
+                if (index !== -1) {
+                    cart[index].quantity++;
+                    cart[index].subtotal = cart[index].price * cart[index].quantity;
+                    cart[index].subtotalWithDiscount = cart[index].price * cart[index].quantity; //És el que mostrem al modal
+                } else {
+
+                    let nouObjecte = new Carts(cartList[i].name, cartList[i].price, cartList[i].type, 1, cartList[i].price, cartList[i].price);
+                    cart.push(nouObjecte);
+                }
+            }
+
+            matriuLengthFirst = cartList.length; //Per saber on començar si afegim nous productes
+            // console.log(matriuLengthFirst); 
+
+        } else { //Quan la matriu Cart ja té alguns articles dins 
+
+            for (let i = matriuLengthFirst; i < cartList.length; i++) {
+
+                let nameMatriu = cartList[i].name;
+
+                const index = cart.findIndex(element => {
+                    if (element.name === nameMatriu) {
+                        return true;
+                    } else
+                        return false;
+                });
+
+                if (index !== -1) {
+
+                    cart[index].quantity++;
+                    cart[index].subtotal = cart[index].price * cart[index].quantity;
+                    cart[index].subtotalWithDiscount = cart[index].price * cart[index].quantity; //És el que mostrem al modal
+                    matriuLengthFirst++;
+
+                } else {
+
+                    let nouObjecte = new Carts(cartList[i].name, cartList[i].price, cartList[i].type, 1, cartList[i].price, cartList[i].price);
+                    cart.push(nouObjecte);
+                    matriuLengthFirst++
+                }
+            }
+
+           // console.log(cart);
+        }
+
+        document.getElementById("count_product").innerHTML = `${cartList.length}`;
+
+        applyPromotionsCart(); //Apliquem promoció amb la funció de l´exercici 5
+
+    } else alert("No products selected. Can´t generate Cart."); //Si cartList és buida
+}
+
+
+
+// Exercise 5 --------------------- Apply promotions to each item in the array "cart"------------------
+
+
+//---- Si l'usuari compra 3 o més ampolles d'oli, el preu del producte descendeix a 10 euros:
+
+
+function applyPromotionsCart() {
+
+    for (i = 0; i < cart.length; i++) {
+
+        if (cart[i].name == 'cooking oil' && cart[i].quantity >= 3) {
+
+            cart[i].subtotalWithDiscount = 10 * cart[i].quantity //preu amb descompte 10euros
+        }
+    }
+
+    //---- Quan es compren 10 o més productes per a fer pastís, el seu preu es rebaixa a 2/3:
+
+    for (i = 0; i < cart.length; i++) {
+
+        if (cart[i].name == 'Instant cupcake mixture' && cart[i].quantity >= 10) {
+
+            cart[i].subtotalWithDiscount = (2 / 3) * 5 * cart[i].quantity;
+        }
+    }
+}
+
+// Exercise 6 --------------------- Fill the shopping cart modal manipulating the shopping cart dom -----------
+
+
+
+function printCart() {
+
+    let comptador = 1;
+    let totalPrice = 0;
+
+    for (i = 0; i < cart.length; i++) {
+
+        document.getElementById(`producte${comptador}`).innerHTML = cart[i].name;
+        document.getElementById(`preu${comptador}`).innerHTML = "$" + cart[i].price;
+        document.getElementById(`quantitat${comptador}`).innerHTML = cart[i].quantity;
+        document.getElementById(`total${comptador}`).innerHTML = "$" + cart[i].subtotalWithDiscount.toFixed(2);
+        comptador++
+        totalPrice += cart[i].subtotalWithDiscount;
+        console.log(totalPrice);
+    }
+    document.getElementById("total_price").innerHTML = totalPrice.toFixed(2);
+}
+
+
+// Exercise 8 ---------------------------------------------------------------------------------------
+
+// Refactor previous code in order to simplify it 
+// 1. Loop for to the array products to get the item to add to cart
+// 2. Add found product to the cart array or update its quantity in case it has been added previously.
+
+function addToCart(id) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ************************************************ Nivell II *****************************************************
+
+
+// Exercise 8
+function removeFromCart(id) {
+    // 1. Loop for to the array products to get the item to add to cart
+    // 2. Add found product to the cartList array
+}
+
+function open_modal() {
+    console.log("Open Modal");
+    printCart();
+}
